30e61ea3318585442e782122adfc166af606b943
brian-n <bhn5ger@virginia.edu>
2023-04-17 16:44:06 -0400

    Created the Binance Socket Manager and testing it using the API

--- market_data_stream_handler.py
+++ market_data_stream_handler.py
@@ -3,5 +3,11 @@ import sqlalchemy
 from binance.client import Client 
 from binance import BinanceSocketManager
 
+client = Client(api_key, api_secret)
+bsm = BinanceSocketManager(client)
+socket = bsm.trade_socket('BTCUSDT')
 
+await socket.__aenter__()
+msg = await socket.recv()
+print(msg)
 
