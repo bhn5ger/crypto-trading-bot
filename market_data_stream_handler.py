c81a45899847f8f64097024f26e7c39aebc57768
brian-n <bhn5ger@virginia.edu>
2023-04-18 05:05:38 -0400

    Storing price data in a df

--- market_data_stream_handler.py
+++ market_data_stream_handler.py
@@ -8,8 +8,11 @@ from binance import BinanceSocketManager
 API_KEY = os.environ.get('binance_api')
 API_SECRET = os.environ.get('binance_secret')
 
+
+msg = None
+
 async def main():
-    
+
     client = Client(API_KEY, API_SECRET)    
     bsm = BinanceSocketManager(client)
     socket = bsm.trade_socket('BTCUSDT')
@@ -17,7 +20,18 @@ async def main():
     async with socket as ts:
 
         msg = await ts.recv()
-        print(msg)
+        return msg
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+
+def createframe():
+    loop = asyncio.get_event_loop()
+    msg = loop.run_until_complete(main())
+    df = pd.DataFrame([msg])
+    df = df.loc[:,['s', 'E', 'p']]
+    df.columns = ['symbol', 'Time', 'Price']
+    df.Price = df.Price.astype(float)
+    df.Time = pd.to_datetime(df.Time, unit = 'ms')
+    return df
+
+
+print(createframe())
