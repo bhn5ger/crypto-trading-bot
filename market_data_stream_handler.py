05f2cb57688437142c21deb1aeb3f78d4f44c7c5
brian-n <bhn5ger@virginia.edu>
2023-04-18 16:16:49 -0400

    Store data stream to DB

--- market_data_stream_handler.py
+++ market_data_stream_handler.py
@@ -8,23 +8,6 @@ from binance import BinanceSocketManager
 API_KEY = os.environ.get('binance_api')
 API_SECRET = os.environ.get('binance_secret')
 
-print(API_KEY)
-print(API_SECRET)
-
-async def main():
-
-    client = Client(API_KEY, API_SECRET)    
-    bsm = BinanceSocketManager(client)
-    socket = bsm.trade_socket('BTCUSDT')
-
-    async with socket as ts:
-        msg = await ts.recv()
-        return msg
-
-
-loop = asyncio.get_event_loop()
-msg = loop.run_until_complete(main())
-
 def createframe(msg):
 
     df = pd.DataFrame([msg])
@@ -34,4 +17,22 @@ def createframe(msg):
     df.Time = pd.to_datetime(df.Time, unit = 'ms')
     return df
 
-print(createframe(msg))
+async def main():
+
+    client = Client(API_KEY, API_SECRET)    
+    bsm = BinanceSocketManager(client)
+    socket = bsm.trade_socket('BTCUSDT')
+
+    async with socket as ts:
+        while True:
+            msg = await ts.recv()
+            frame = createframe(msg)
+            frame.to_sql('BTCUSDT', engine, if_exists='append', index=False)
+            print(frame)
+
+engine = sqlalchemy.create_engine('sqlite:///BTCUSDTstream.db')
+    
+loop = asyncio.get_event_loop()
+msg = loop.run_until_complete(main())
+
+
