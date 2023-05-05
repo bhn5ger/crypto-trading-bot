efe55b12cb3b5a9847fbb47fd82465f84bc5c0bf
brian-n <bhn5ger@virginia.edu>
2023-05-05 06:51:31 -0400

    Get USDT coin pairs from the relevant symbols

--- all_coin_data_stream_handler.py
+++ all_coin_data_stream_handler.py
@@ -17,9 +17,8 @@ info = client.get_exchange_info()
 symbols = [x['symbol'] for x in info['symbols']]
 exclude = ['UP', 'DOWN', 'BEAR', 'BULL']
 non_lev = [symbol for symbol in symbols if all(excludes not in symbol for excludes in exclude)]
-
-print(non_lev)
-
+relevant = [symbol for symbol in non_lev if symbol.endswith('USDT')]
+print(relevant)
 
 
 
