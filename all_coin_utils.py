db4044747f9a7ed40776002cbf2e5f45414288ec
brian-n <bhn5ger@virginia.edu>
2023-05-05 07:28:49 -0400

    ls

---
+++ all_coin_utils.py
@@ -0,0 +1,19 @@
+import asyncio
+from binance import AsyncClient, BinanceSocketManager
+import pandas as pd
+import datetime as dt
+import os
+import sqlalchemy
+from binance.client import Client # not necessary?
+
+API_KEY = os.environ.get('binance_api')
+API_SECRET = os.environ.get('binance_secret')
+
+client = Client(API_KEY, API_SECRET, tld='us') # not necessary?
+
+engine = sqlalchemy.create_engine('sqlite:///CryptoDB.db')
+
+symbols = pd.read_sql('SELECT name FROM sqlite_master WHERE type="table"', engine)
+
+
+
