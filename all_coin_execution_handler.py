4229421854d0ddf9a1bdaa7f347122c0345128fb
brian-n <bhn5ger@virginia.edu>
2023-05-06 19:55:45 -0400

    Sell order condition

--- all_coin_execution_handler.py
+++ all_coin_execution_handler.py
@@ -1,6 +1,9 @@
-import os
+import os, asyncio
+import pandas as pd
 import all_coin_utils, account_utils
 from binance.client import Client 
+from binance import AsyncClient, BinanceSocketManager
+
 
 API_KEY = os.environ.get('binance_api')
 API_SECRET = os.environ.get('binance_secret')
@@ -9,27 +12,65 @@ client = Client(API_KEY, API_SECRET, tld='us')
 
 # lookback is in minutes
 
-def all_coin_strategy(lookback):
+def all_coin_strategy(lookback, investment_amt):
 
     top_coin = all_coin_utils.get_coin_with_greatest_cumulative_returns_in_past_n_minutes(lookback)
-    investment_amt = 300
     lot_size = all_coin_utils.get_minimum_permitted_investment_qty(top_coin)
-    price = float(client.get_symbol_ticker(symbol=top_coin)['price'])
-    buy_quantity = round(investment_amt/price, len(str(lot_size).split('.')[1]))
     free_usd = account_utils.get_available_balance('USDT')
 
+    price = float(client.get_symbol_ticker(symbol=top_coin)['price'])
+    buy_quantity = round(investment_amt/price, len(str(lot_size).split('.')[1]))
+
     if float(free_usd) > investment_amt:
+
         order = client.create_order(
                                     symbol=top_coin, 
                                     side='BUY',
                                     type='MARKET',
                                     quantity=buy_quantity
                                    )
+        
+        buyprice = float(order['fills'][0]['price'])
+        
+    else:
+        print('order has not been executed')
+        quit()
 
-    print(price)
+def create_frame(msg):
+
+    df = pd.DataFrame([msg])
+    df = df.loc[:,['s','E','p']]
+    df.columns = ['symbol', 'Time', 'Price']
+    df.Price = df.Price.astype(float)
+    df.Time = pd.to_datetime(df.Time, unit='ms')
+    return df
+
+async def main(coin, buyprice, buy_quantity):
+
+    bm = BinanceSocketManager(client)
+    ts = bm.trade_socket(coin)
+
+    async with ts as tscm:
+        while True:
+            msg = await tscm.recv()
+            if msg:
+                frame = create_frame(msg)
+                if frame.Price[0] < buyprice * 0.97 or frame.Price[0] > 1.005 * buyprice:
+                    order = client.create_order(
+                                                symbol=coin,
+                                                side='SELL',
+                                                type='MARKET',
+                                                quantity=buy_quantity
+                                               )
+                    print(order)
+                    loop.stop()
+
+    await client.close_connection()
+    
+loop = asyncio.get_event_loop()
+msg = loop.run_until_complete(main())
 
 #all_coin_strategy(6000)
-print(account_utils.get_available_balance('USDT'))
 
 
 
--- api_testing.py
+++ api_testing.py
@@ -1,6 +1,9 @@
+import asyncio
 import os
 import pandas as pd
 from binance.client import Client 
+from binance import AsyncClient, BinanceSocketManager
+
 
 API_KEY = os.environ.get('binance_api')
 API_SECRET = os.environ.get('binance_secret')
@@ -10,30 +13,20 @@ client = Client(API_KEY, API_SECRET, tld='us')
 '''
 BUY RESPONSE
 
-{'symbol': 'BTCUSDT', 'orderId': 988476733, 'orderListId': -1, 'clientOrderId': '5GbaUurWYkpCEHTSjPvoxN', 
-'transactTime': 1681984758356, 'price': '0.00000000', 'origQty': '0.00010000', 'executedQty': '0.00010000', 
-'cummulativeQuoteQty': '2.88652000', 'status': 'FILLED', 'timeInForce': 'GTC', 'type': 'MARKET', 
-'side': 'BUY', 'workingTime': 1681984758356, 'fills': [{'price': '28865.20000000', 'qty': '0.00010000', 
-'commission': '0.00000000', 'commissionAsset': 'BNB', 'tradeId': 22187132}], 'selfTradePreventionMode': 
+{'symbol': 'BTCUSDT', 'orderId': 1005200732, 'orderListId': -1, 'clientOrderId': 'HHk7NO6aPVikgxYnrzKlfN', 
+'transactTime': 1683392507234, 'price': '0.00000000', 'origQty': '0.00300000', 'executedQty': '0.00300000',
+'cummulativeQuoteQty': '86.05938000', 'status': 'FILLED', 'timeInForce': 'GTC', 'type': 'MARKET', 
+'side': 'BUY', 'workingTime': 1683392507234, 'fills': [{'price': '28686.46000000', 'qty': '0.00300000', 
+'commission': '0.00000000', 'commissionAsset': 'BNB', 'tradeId': 23006532}], 'selfTradePreventionMode': 
 'EXPIRE_MAKER'}
+
 '''
 
-while True:
+order = client.create_order(
+                            symbol='BTCUSDT',
+                            side=Client.SIDE_SELL,
+                            type=Client.ORDER_TYPE_MARKET,
+                            quantity=0.003                                      
+                        )
 
-    btc_price = client.get_symbol_ticker(symbol="BTCUSDT")
-    
-    print(float(btc_price['price']))
-
-    if float(btc_price['price']) > 29000:
-
-        order = client.create_order(
-                                    symbol='BTCUSDT',
-                                    side=Client.SIDE_BUY,
-                                    type=Client.ORDER_TYPE_MARKET,
-                                    quantity=0.0001                                             
-                                )
-
-        print(order)
-
-        break
-
+print(order)
