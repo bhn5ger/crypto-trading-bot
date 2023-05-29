d91b66c0df8ff89342a86239d62e965ce29b5adf
bhn5ger <bhn5ger@virginia.edu>
2023-05-29 21:31:14 +0000

    Get all USDT symbols

--- trading_platform/data_stream/all_coin_data_stream_handler_csv.py
+++ trading_platform/data_stream/all_coin_data_stream_handler_csv.py
@@ -7,7 +7,8 @@ path = '/home/briann.briannn/crypto-trading-bot/trading_platform/data_stream/csv
 stream = "wss://stream.binance.com:9443/ws/!miniTicker@arr"
 
 def on_message(ws, message):
-    msg = json.loads()
+    msg = json.loads(message)
+    symbol = [x for x in msg if x['s'].endswith('USDT')]
 
 ws = websocket.WebSocketApp(stream, on_message=on_message)
 ws.run_forever()
