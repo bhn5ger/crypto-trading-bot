afb4678322e785e21589cd5158b61fe18be0e6e2
bhn5ger <bhn5ger@virginia.edu>
2023-05-29 22:10:22 +0000

    Fix file path for csv data

--- trading_platform/data_stream/all_coin_data_stream_handler_csv.py
+++ trading_platform/data_stream/all_coin_data_stream_handler_csv.py
@@ -2,7 +2,7 @@ import websocket
 import json
 import pandas as pd
 
-path = '/home/briann.briannn/crypto-trading-bot/trading_platform/data_stream/csvs'
+path = '/home/briann.briannn/crypto-trading-bot/trading_platform/data_stream/csvs/'
 
 stream = "wss://stream.binance.com:9443/ws/!miniTicker@arr"
 
@@ -10,7 +10,7 @@ def on_message(ws, message):
 
     msg = json.loads(message)
     symbol = [x for x in msg if x['s'].endswith('USDT')]
-    
+
     frame = pd.DataFrame(symbol)[['E','s','c']]
     frame.E = pd.to_datetime(frame.E, unit='ms')
     frame.c = frame.c.astype(float)
