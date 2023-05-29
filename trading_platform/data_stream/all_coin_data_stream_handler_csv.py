0916c938c0a94180ac7637fbcdc10d1f2605a95a
bhn5ger <bhn5ger@virginia.edu>
2023-05-29 21:34:20 +0000

    Convert the USDT live websocket price data into a dataframe with proper units

--- trading_platform/data_stream/all_coin_data_stream_handler_csv.py
+++ trading_platform/data_stream/all_coin_data_stream_handler_csv.py
@@ -9,6 +9,9 @@ stream = "wss://stream.binance.com:9443/ws/!miniTicker@arr"
 def on_message(ws, message):
     msg = json.loads(message)
     symbol = [x for x in msg if x['s'].endswith('USDT')]
+    frame = pd.DataFrame(symbol)[['E','s','c']]
+    frame.E = pd.to_datetime(frame.E, unit='ms')
+    frame.c = frame.c.astype(float)
 
 ws = websocket.WebSocketApp(stream, on_message=on_message)
 ws.run_forever()
