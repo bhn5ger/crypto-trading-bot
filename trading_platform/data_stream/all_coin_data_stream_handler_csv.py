5c1789343ea147c93f16aa5ebdbd82e325a96aa1
bhn5ger <bhn5ger@virginia.edu>
2023-05-29 21:30:04 +0000

    Start using websockets instead of APIs to optimize network latency

--- trading_platform/data_stream/all_coin_data_stream_handler_csv.py
+++ trading_platform/data_stream/all_coin_data_stream_handler_csv.py
@@ -2,6 +2,10 @@ import websocket
 import json
 import pandas as pd
 
+path = '/home/briann.briannn/crypto-trading-bot/trading_platform/data_stream/csvs'
+
+stream = "wss://stream.binance.com:9443/ws/!miniTicker@arr"
+
 def on_message(ws, message):
     msg = json.loads()
 
